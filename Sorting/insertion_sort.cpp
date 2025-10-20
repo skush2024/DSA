fe60d540c5a966785d3077fab568a6ef1332b519
skush2024 <kushashah50@gmail.com>
2025-10-20 12:41:43 -0700

    Added Insertion Sort Implementation

---
+++ Sorting/insertion_sort.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+
+// Calling Insertion sort by reference since its an array.
+void insertion_sort(int arr[], int n){
+    for(int i = 1; i < n; i++){
+        for(int j = i; j > 0 ; j--){
+            if (arr[j] < arr[j - 1]){
+                int temp = arr[j];
+                arr[j] = arr[j - 1];
+                arr[j - 1] = temp;
+            }
+        }
+    }
+}
+
+
+// Implementing insertion sort by reference using pointers.
+void insertion_sort_ptr(int* arr, int n){
+    for(int i = 1; i < n; i++){
+        for(int j = i; j > 0 ; j--){
+            if (*(arr + j) < *(arr + j - 1)){
+                int temp = *(arr + j);
+                *(arr + j) = *(arr + j - 1);
+                *(arr + j - 1) = temp;
+            }
+        }
+    }
+}
+
+
+
+
+int main(){
+    int num[] = {13,46,24,52,20,9};
+    int n = sizeof(num) / sizeof(num[0]);
+
+    insertion_sort_ptr(num,n);
+    for(int i = 0; i < n; i++){
+        cout << num[i] << endl;
+    }
+    return 0;
+}
